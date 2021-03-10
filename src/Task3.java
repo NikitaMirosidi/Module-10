454b145c4de0c0097316087ad02140e8056fcf24
Nikita <dr_mario@ua.fm>
2021-03-11 01:38:17 +0200

    initial commit

--- src/Task3.java
+++ src/Task3.java
@@ -1,10 +1,12 @@
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Task3 {
     public String method(List<String> list) {
         return list.stream()
-                .map(s -> s.replaceAll("\\D+", ""))
+                .map(s -> s.split("\\D+"))
+                .flatMap(Arrays::stream)
                 .sorted()
                 .collect(Collectors.joining(", ", "\"", "\""));
     }
